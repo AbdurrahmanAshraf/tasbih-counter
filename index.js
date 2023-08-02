8402e771dadc4317fb6227da349bb77cad8b1653
Abdurrahman Ashraf <65561308+AbdurrahmanAshraf@users.noreply.github.com>
2023-08-02 11:20:17 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+let counter = 0
+count.textContent = counter
+
+function increment() {
+  counter += 1
+  count.textContent = counter
+}
+
+function reset(){
+  counter = 0
+  count.textContent = counter
+}
